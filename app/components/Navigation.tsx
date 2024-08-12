609bd9ed8c30ea7414c410e2ce766762b2d6ff3a
RunesofYggdrasil <rookmarcus79@gmail.com>
2024-08-12 18:17:16 -0400

    Scrollbar Width Working

---
+++ app/components/Navigation.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import React, { useEffect } from "react";
+import styles from "./Navigation.module.css";
+import Link from "next/link";
+
+const Navigation = () => {
+  useEffect(() => {
+    function handlePageWidth() {
+      const root = document.documentElement;
+      const innerWidth = window.innerWidth;
+      const scrollbarWidth = innerWidth - root.clientWidth;
+      const pageWidth = innerWidth - scrollbarWidth - 16;
+      root.style.setProperty("--page-width", pageWidth + "px");
+    }
+
+    handlePageWidth();
+    window.addEventListener("resize", () => {
+      handlePageWidth();
+    });
+    window.addEventListener("scroll", () => {
+      handlePageWidth();
+    });
+    return () => {
+      window.removeEventListener("resize", () => {
+        handlePageWidth();
+      });
+      window.removeEventListener("scroll", () => {
+        handlePageWidth();
+      });
+    };
+  });
+  return (
+    <nav className={styles.navigationOutside}>
+      <div className={styles.navigationInside}>
+        <div className={styles.navigationLeft}>
+          <p>CROWSEE</p>
+        </div>
+        <div className={styles.navigationLeft}>
+          <ul>
+            <li>
+              <Link href="./">HOME</Link>
+            </li>
+            <li>
+              <Link href="./">HOME</Link>
+            </li>
+            <li>
+              <Link href="./">HOME</Link>
+            </li>
+            <li>
+              <Link href="./">HOME</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
