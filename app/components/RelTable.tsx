28a066afb58206130d68a8ad323c4925c5148b1a
RunesofYggdrasil <rookmarcus79@gmail.com>
2024-09-26 15:18:59 -0400

    Updated RelTable to Either Display Loading or Table

--- app/components/RelTable.tsx
+++ app/components/RelTable.tsx
@@ -3,19 +3,24 @@
 import React, { useEffect, useState } from "react";
 import RelTypeButton from "./RelTypeButton";
 import fetchAPI from "../api/fetch";
+import Loading from "./Loading";
 
 interface RelTableProps {
-  characters: {
-    id: number;
-    firstName: string;
-    lastName: string;
-  }[];
-  reltypes: {
-    id: number;
-    name: string;
-    hexCode: string;
-    textCode: string;
-  }[];
+  characters:
+    | {
+        id: number;
+        firstName: string;
+        lastName: string;
+      }[]
+    | undefined;
+  reltypes:
+    | {
+        id: number;
+        name: string;
+        hexCode: string;
+        textCode: string;
+      }[]
+    | undefined;
 }
 
 type Vote = {
@@ -44,86 +49,90 @@ function handleSubmitGatherVotes(
 }
 
 const RelTable = ({ characters, reltypes }: RelTableProps) => {
-  const tableButtons: [
-    number,
-    React.Dispatch<React.SetStateAction<number>>
-  ][][] = [];
-  for (let i = 0; i < characters.length; i++) {
-    const tableButtonsRow = [];
-    for (let j = i; j < characters.length; j++) {
-      const tableButtonIndex = useState(0);
-      tableButtonsRow.push(tableButtonIndex);
+  if (characters && reltypes) {
+    const tableButtons: [
+      number,
+      React.Dispatch<React.SetStateAction<number>>
+    ][][] = [];
+    for (let i = 0; i < characters.length; i++) {
+      const tableButtonsRow = [];
+      for (let j = i; j < characters.length; j++) {
+        const tableButtonIndex = useState(0);
+        tableButtonsRow.push(tableButtonIndex);
+      }
+      tableButtons.push(tableButtonsRow);
     }
-    tableButtons.push(tableButtonsRow);
-  }
-  return (
-    <>
-      <table>
-        <tbody>
-          <tr>
-            <th>CROWSEE</th>
-            {characters.map((character) => {
+    return (
+      <>
+        <table>
+          <tbody>
+            <tr>
+              <th>CROWSEE</th>
+              {characters.map((character) => {
+                return (
+                  <th key={"h" + character.id}>
+                    {character.firstName} {character.lastName.substring(0, 1)}.
+                  </th>
+                );
+              })}
+            </tr>
+            {characters.map((characterTwo, indexTwo) => {
               return (
-                <th key={"h" + character.id}>
-                  {character.firstName} {character.lastName.substring(0, 1)}.
-                </th>
+                <tr key={"r" + characterTwo.id}>
+                  <th>
+                    {characterTwo.firstName}{" "}
+                    {characterTwo.lastName.substring(0, 1)}.
+                  </th>
+                  {characters.map((characterOne, indexOne) => {
+                    const [currentIndex, setCurrentIndex] =
+                      indexOne >= indexTwo
+                        ? tableButtons[indexTwo][indexOne - indexTwo]
+                        : tableButtons[indexOne][indexTwo - indexOne];
+                    return (
+                      <td key={"d" + characterOne.id}>
+                        <RelTypeButton
+                          index={{ currentIndex, setCurrentIndex }}
+                          position={{
+                            x: indexOne,
+                            y: indexTwo,
+                            id1: characterOne.id,
+                            id2: characterTwo.id,
+                          }}
+                          reltypes={reltypes}
+                        />
+                      </td>
+                    );
+                  })}
+                </tr>
               );
             })}
-          </tr>
-          {characters.map((characterTwo, indexTwo) => {
-            return (
-              <tr key={"r" + characterTwo.id}>
-                <th>
-                  {characterTwo.firstName}{" "}
-                  {characterTwo.lastName.substring(0, 1)}.
-                </th>
-                {characters.map((characterOne, indexOne) => {
-                  const [currentIndex, setCurrentIndex] =
-                    indexOne >= indexTwo
-                      ? tableButtons[indexTwo][indexOne - indexTwo]
-                      : tableButtons[indexOne][indexTwo - indexOne];
-                  return (
-                    <td key={"d" + characterOne.id}>
-                      <RelTypeButton
-                        index={{ currentIndex, setCurrentIndex }}
-                        position={{
-                          x: indexOne,
-                          y: indexTwo,
-                          id1: characterOne.id,
-                          id2: characterTwo.id,
-                        }}
-                        reltypes={reltypes}
-                      />
-                    </td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <button
-        type="submit"
-        onClick={() => {
-          const votes = handleSubmitGatherVotes(tableButtons, characters);
-          useEffect(() => {
-            const postVoteCall = async () => {
-              const postVoteBody = JSON.stringify({ votes });
-              const postVoteRequest = await fetchAPI(
-                "PUT",
-                "votes",
-                postVoteBody
-              );
-            };
+          </tbody>
+        </table>
+        <button
+          type="submit"
+          onClick={() => {
+            const votes = handleSubmitGatherVotes(tableButtons, characters);
+            useEffect(() => {
+              const postVoteCall = async () => {
+                const postVoteBody = JSON.stringify({ votes });
+                const postVoteRequest = await fetchAPI(
+                  "PUT",
+                  "votes",
+                  postVoteBody
+                );
+              };
 
-            postVoteCall();
-          });
-        }}
-      >
-        SUBMIT
-      </button>
-    </>
-  );
+              postVoteCall();
+            });
+          }}
+        >
+          SUBMIT
+        </button>
+      </>
+    );
+  } else {
+    return <Loading />;
+  }
 };
 
 export default RelTable;
