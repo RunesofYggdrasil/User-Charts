058045b0dd81344c59fd157cd1e4db0ceae3ee73
RunesofYggdrasil <rookmarcus79@gmail.com>
2024-09-27 10:34:34 -0400

    Loading Updates

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./Loading.module.css";
 
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className={styles.loadDiv}>
+      <p>Loading...</p>
+    </div>
+  );
 };
 
 export default Loading;
